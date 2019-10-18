00d63b4514c25df6a538fb2a9840e45c31f82c91
Zhiyun Ling <zhiyunling@ufl.edu>
2019-10-17 23:06:20 -0400

    Finished Longest Common Prefix

---
+++ longestCommonPrefix.py
@@ -0,0 +1,39 @@
+"""
+write a function to find the longest common prefix string amongst an array of strings.
+input: [str1, str2, str3 ....], strings are *all* lowercase a-z
+output: string
+extreme case: If no common prefix return ""
+        if empty array/ None, return ""
+        if empty string, return ""
+
+note: *common to all*
+first, find minimum length
+traverse through all array, compare first element
+second,continue to minimum length of elements
+Until null/diff
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        if strs is None or strs==[]:
+            return ""
+        common = ""
+        minlen=len(strs[0])
+        #  get min length
+        arr_len=len(strs)
+        for i in range(arr_len):
+            if strs[i] is None or strs[i] == "":
+                return ""
+            else:
+                minlen=min(minlen,len(strs[i]))
+
+        for i in range(minlen):
+            for j in range(1,arr_len):
+                if strs[j][i]!=strs[0][i]:
+                    return common
+            common+=strs[0][i]
+        return common
+
+#can use devide and conquer
+#can use binary search
+# use sorted can improve efficiency
--- main.py
+++ main.py
@@ -9,16 +9,31 @@
 # print(sl.countAndSay(12))
 # print("time is %.5f"%((time.time()-start)*10000))
 
-from defangIPaddr import Solution
+# from defangIPaddr import Solution
+# import time
+#
+# N = 20
+# start = time.time()
+# for i in range(N):
+#     sl = Solution()
+#     # str = "192.168.1.1"
+#     str = "0.0.0.1"
+#     sl.defangIPaddr(str)
+#
+# print("time is %.5f"%((time.time()-start)*10000/N))
+# print(sl.defangIPaddr(str))
+
+
+from longestCommonPrefix import Solution
 import time
 
 N = 20
 start = time.time()
+sl = Solution()
+list = ["flower", "flow", "flight"]
+list=["dog","racecar","car"]
 for i in range(N):
-    sl = Solution()
-    # str = "192.168.1.1"
-    str = "0.0.0.1"
-    sl.defangIPaddr(str)
+    sl.longestCommonPrefix(list)
 
 print("time is %.5f"%((time.time()-start)*10000/N))
-print(sl.defangIPaddr(str))
+print(sl.longestCommonPrefix(list))
